348ff3bc469a5a4ba3f486de3f33ff2c9ac7d32e
Betelihem <Betelihem>
2022-04-12 14:49:47 -0700

    commit tree 8716fb472be125313a90b7475c3fb6dea1a99a6d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :leases, only:[:create, :destroy]
+  resources :apartments #, only: [:create, :show, :index, :update, :destroy]
+  resources :tenants #, only: [:create, :show, :index, :update, :destroy]
 end
